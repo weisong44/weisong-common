1821e1fa540067e11dc294d037259c4ba8408f5d
Wei Song <weisong44@hotmail.com>
2015-02-25 16:45:05 -0800

    Added hooks for MBean naming stragegy

--- src/main/java/com/weisong/common/javaconfig/CommonJmxJavaConfig.java
+++ src/main/java/com/weisong/common/javaconfig/CommonJmxJavaConfig.java
@@ -1,17 +1,26 @@
 package com.weisong.common.javaconfig;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.annotation.AnnotationMBeanExporter;
+import org.springframework.jmx.export.naming.ObjectNamingStrategy;
 import org.springframework.jmx.support.RegistrationPolicy;
 
 @Configuration
 public class CommonJmxJavaConfig {
+	
+	@Autowired(required = false)
+	private ObjectNamingStrategy namingStrategy;
+	
     @Bean
     public AnnotationMBeanExporter annotationMBeanExporter() {
         AnnotationMBeanExporter exporter = new AnnotationMBeanExporter();
         exporter.setRegistrationPolicy(RegistrationPolicy.REPLACE_EXISTING);
+        if(namingStrategy != null) {
+        	exporter.setNamingStrategy(namingStrategy);
+        }
         return exporter;
     }
 }
